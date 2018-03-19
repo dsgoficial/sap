65067a69fec38c97abb12c7a2154d3916696f5b0
Felipe Diniz <diniz.ime@gmail.com>
2018-03-19 18:20:59 -0300

    Correção trigger view monitoramento

--- er/monitoramento.sql
+++ er/monitoramento.sql
@@ -5,4 +5,6 @@ CREATE SCHEMA monitoramento;
 GRANT USAGE ON SCHEMA monitoramento TO public;
 GRANT SELECT ON ALL TABLES IN SCHEMA monitoramento TO public;
 
+GRANT ALL ON schema monitoramento TO controle_app;
+
 COMMIT;
