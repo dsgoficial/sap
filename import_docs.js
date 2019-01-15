4ad80e4e4c770c959427dc28f850592ef6d6ec0a
Felipe Diniz <diniz.ime@gmail.com>
2019-01-15 14:15:23 -0200

    import docs

---
+++ import_docs.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const promise = require("bluebird");
+
+const initOptions = {
+  promiseLib: promise
+};
+
+const pgp = require("pg-promise")(initOptions);
+
+const createConfig = () => {
+  console.log(chalk.blue("Importação em lote do DSGDocs"));
+
+  var questions = [
+    {
+      type: "input",
+      name: "docs_url",
+      message: "Qual o endereço do DSGDocs?"
+    },
+    {
+      type: "input",
+      name: "categoria",
+      message: "Qual a categoria de arquivos que se deseja importar?"
+    }
+  ];
+
+  inquirer.prompt(questions).then(async answers => {
+
+    try {
+
+        console.log(chalk.blue("Arquivos importados com sucesso!"));
+
+    } catch (error) {
+      console.log(chalk.red(error.message));
+      console.log("-------------------------------------------------");
+      console.log(chalk.red(error));
+    }
+  });
+};
+
+createConfig();
