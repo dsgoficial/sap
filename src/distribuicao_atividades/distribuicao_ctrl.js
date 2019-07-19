57372be6541595cc1233504c70b0dad4364f6252
Felipe Diniz <diniz.ime@gmail.com>
2019-07-19 06:32:19 -0300

    fix carta pausada

--- src/distribuicao_atividades/distribuicao_ctrl.js
+++ src/distribuicao_atividades/distribuicao_ctrl.js
@@ -99,11 +99,8 @@ const calculaFila = async usuario => {
       let cartas_pausadas = await t.oneOrNone(
         `SELECT etapa_id, unidade_trabalho_id
         FROM (
-        SELECT ee.etapa_id, ee.unidade_trabalho_id, ee_ant.tipo_situacao_id AS situacao_ant, lo.prioridade AS lo_prioridade, pse.prioridade AS pse_prioridade, ut.prioridade AS ut_prioridade
+        SELECT ee.etapa_id, ee.unidade_trabalho_id, ee_ant.tipo_situacao_id AS situacao_ant, lo.prioridade AS lo_prioridade, ut.prioridade AS ut_prioridade
         FROM macrocontrole.atividade AS ee
-        INNER JOIN macrocontrole.perfil_producao_etapa AS pse ON pse.etapa_id = ee.etapa_id
-        INNER JOIN macrocontrole.perfil_producao_operador AS ppo ON ppo.perfil_producao_id = pse.perfil_producao_id
-        INNER JOIN dgeo.usuario AS u ON u.id = ppo.usuario_id
         INNER JOIN macrocontrole.etapa AS se ON se.id = ee.etapa_id
         INNER JOIN macrocontrole.unidade_trabalho AS ut ON ut.id = ee.unidade_trabalho_id
         INNER JOIN macrocontrole.lote AS lo ON lo.id = ut.lote_id
@@ -119,58 +116,19 @@ const calculaFila = async usuario => {
         AND ee.id NOT IN
         (
           SELECT a.id FROM macrocontrole.atividade AS a
-          INNER JOIN macrocontrole.perfil_producao_etapa AS ppe ON ppe.etapa_id = a.etapa_id
-          INNER JOIN macrocontrole.perfil_producao_operador AS ppo ON ppo.perfil_producao_id = ppe.perfil_producao_id
           INNER JOIN macrocontrole.unidade_trabalho AS ut ON ut.id = a.unidade_trabalho_id
           INNER JOIN macrocontrole.pre_requisito_subfase AS prs ON prs.subfase_posterior_id = ut.subfase_id
           INNER JOIN macrocontrole.unidade_trabalho AS ut_re ON ut_re.subfase_id = prs.subfase_anterior_id
           INNER JOIN macrocontrole.atividade AS a_re ON a_re.unidade_trabalho_id = ut_re.id
-          WHERE ppo.usuario_id = $1 AND prs.tipo_pre_requisito_id = 1 AND 
+          WHERE ee.usuario_id = $1 AND prs.tipo_pre_requisito_id = 1 AND 
           ut.geom && ut_re.geom AND
           st_relate(ut.geom, ut_re.geom, '2********') AND
           a_re.tipo_situacao_id IN (1, 2, 3)
         )
-        AND ee.id NOT IN
-        (
-          SELECT a.id FROM macrocontrole.atividade AS a
-          INNER JOIN macrocontrole.perfil_producao_etapa AS ppe ON ppe.etapa_id = a.etapa_id
-          INNER JOIN macrocontrole.perfil_producao_operador AS ppo ON ppo.perfil_producao_id = ppe.perfil_producao_id
-          INNER JOIN macrocontrole.etapa AS et ON et.id = a.etapa_id
-          INNER JOIN dgeo.usuario AS u ON u.id = ppo.usuario_id
-          INNER JOIN macrocontrole.unidade_trabalho AS ut ON ut.id = a.unidade_trabalho_id
-          INNER JOIN macrocontrole.restricao_etapa AS re ON re.etapa_posterior_id = a.etapa_id
-          INNER JOIN macrocontrole.atividade AS a_re ON a_re.etapa_id = re.etapa_anterior_id
-          INNER JOIN macrocontrole.etapa AS et_re ON et_re.id = a_re.etapa_id
-          INNER JOIN dgeo.usuario AS u_re ON u_re.id = a_re.usuario_id
-          INNER JOIN macrocontrole.unidade_trabalho AS ut_re ON ut_re.id = a_re.unidade_trabalho_id
-          WHERE ppo.usuario_id = $1 AND et_re.subfase_id != et.subfase_id AND st_relate(ut.geom, ut_re.geom, '2********') AND (
-              (re.tipo_restricao_id = 1 AND a_re.usuario_id = $1) OR
-              (re.tipo_restricao_id = 2 AND a_re.usuario_id != $1) OR 
-              (re.tipo_restricao_id = 3 AND u_re.tipo_turno_id != u.tipo_turno_id AND u_re.tipo_turno_id != 3 AND u.tipo_turno_id != 3)
-          )
-        )
-        AND ee.id NOT IN
-        (
-          SELECT ee.id FROM macrocontrole.atividade AS ee
-          INNER JOIN macrocontrole.perfil_producao_etapa AS pse ON pse.etapa_id = ee.etapa_id
-          INNER JOIN macrocontrole.perfil_producao_operador AS ppo ON ppo.perfil_producao_id = pse.perfil_producao_id
-          INNER JOIN macrocontrole.etapa AS et ON et.id = ee.etapa_id
-          INNER JOIN dgeo.usuario AS u ON u.id = ppo.usuario_id
-          INNER JOIN macrocontrole.restricao_etapa AS re ON re.etapa_posterior_id = ee.etapa_id
-          INNER JOIN macrocontrole.atividade AS ee_re ON ee_re.etapa_id = re.etapa_anterior_id
-            AND ee_re.unidade_trabalho_id = ee.unidade_trabalho_id
-          INNER JOIN macrocontrole.etapa AS et_re ON et_re.id = ee_re.etapa_id
-          INNER JOIN dgeo.usuario AS u_re ON u_re.id = ee_re.usuario_id
-          WHERE ppo.usuario_id = $1 AND et_re.subfase_id = et.subfase_id AND (
-            (re.tipo_restricao_id = 1 AND ee_re.usuario_id = $1) OR
-            (re.tipo_restricao_id = 2 AND ee_re.usuario_id != $1) OR 
-            (re.tipo_restricao_id = 3 AND u_re.tipo_turno_id != u.tipo_turno_id AND u_re.tipo_turno_id != 3 AND u.tipo_turno_id != 3)
-          )
-        )
         ) AS sit
-        GROUP BY etapa_id, unidade_trabalho_id, lo_prioridade, pse_prioridade, ut_prioridade
+        GROUP BY etapa_id, unidade_trabalho_id, lo_prioridade, ut_prioridade
         HAVING MIN(situacao_ant) IS NULL OR every(situacao_ant IN (4,5)) 
-        ORDER BY lo_prioridade, pse_prioridade, ut_prioridade
+        ORDER BY lo_prioridade, ut_prioridade
         LIMIT 1`,
         [usuario]
       );
@@ -566,8 +524,13 @@ controller.verifica = async usuario_id => {
       WHERE a.usuario_id = $1 and ut.disponivel IS TRUE and a.tipo_situacao_id = 2 LIMIT 1`,
       [usuario_id]
     );
-
     if (em_andamento) {
+      await db.none(
+        `UPDATE macrocontrole.atividade SET
+          tipo_situacao_id = 3 
+          WHERE tipo_situacao_id = 2 and usuario_id = $1 and etapa_id != $2 and unidade_trabalho_id != $3`,
+        [usuario_id, em_andamento.etapa_id, em_andamento.unidade_trabalho_id]
+      );
       const { erro, dados } = await dadosProducao(
         em_andamento.etapa_id,
         em_andamento.unidade_trabalho_id
