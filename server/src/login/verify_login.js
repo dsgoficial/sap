f50e1f02781b30f7e73820809c823717200578f6
Felipe Diniz <diniz.ime@gmail.com>
2020-08-25 14:07:53 -0300

    small fix

--- server/src/login/verify_login.js
+++ server/src/login/verify_login.js
@@ -1,28 +1,27 @@
-'use strict'
+"use strict";
 
-const { AppError, asyncHandler, httpCode } = require('../utils')
+const { AppError, asyncHandler, httpCode } = require("../utils");
 
-const validateToken = require('./validate_token')
+const validateToken = require("./validate_token");
 
 // middleware para verificar o JWT
 const verifyLogin = asyncHandler(async (req, res, next) => {
   // verifica o header authorization para pegar o token
-  const token = req.headers.authorization
+  const token = req.headers.authorization;
 
-  const decoded = await validateToken(token)
+  const decoded = await validateToken(token);
 
   if (req.params.usuario_uuid && decoded.uuid !== req.params.usuario_uuid) {
     throw new AppError(
-      'Usuário só pode acessar sua própria informação',
+      "Usuário só pode acessar sua própria informação",
       httpCode.Unauthorized
-    )
+    );
   }
 
-  req.usuarioUuid = decoded.uuid
-  req.usuarioId = decoded.id
-  req.administrador = false
-  
-  next()
-})
+  req.usuarioUuid = decoded.uuid;
+  req.usuarioId = decoded.id;
 
-module.exports = verifyLogin
+  next();
+});
+
+module.exports = verifyLogin;
