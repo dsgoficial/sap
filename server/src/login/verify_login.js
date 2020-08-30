5dcc69ef95fe366b3f1aaaa4fb0e6935f2831121
Felipe Diniz <diniz.ime@gmail.com>
2020-08-30 17:14:58 -0300

    fix check

--- server/src/login/verify_login.js
+++ server/src/login/verify_login.js
@@ -2,6 +2,8 @@
 
 const { AppError, asyncHandler, httpCode } = require("../utils");
 
+const { db } = require("../database");
+
 const validateToken = require("./validate_token");
 
 // middleware para verificar o JWT
@@ -11,11 +13,26 @@ const verifyLogin = asyncHandler(async (req, res, next) => {
 
   const decoded = await validateToken(token);
 
+  if (!("id" in decoded && decoded.id)) {
+    throw new AppError("Falta informação de usuário");
+  }
+  if (!decoded.uuid) {
+    throw new AppError("Falta informação de usuário");
+  }
+
   if (req.params.usuario_uuid && decoded.uuid !== req.params.usuario_uuid) {
     throw new AppError(
       "Usuário só pode acessar sua própria informação",
       httpCode.Unauthorized
     );
+  }
+
+  const response = await db.conn.oneOrNone(
+    "SELECT ativo FROM dgeo.usuario WHERE uuid = $<usuarioUuid>",
+    { usuarioUuid: decoded.uuid }
+  );
+  if (!response.ativo) {
+    throw new AppError("Usuário não está ativo", httpCode.Forbidden);
   }
 
   req.usuarioUuid = decoded.uuid;
