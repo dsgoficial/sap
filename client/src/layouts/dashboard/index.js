5c4a0bf50fc01402f718505540fbd79fc815b4d0
Felipe Diniz <diniz.ime@gmail.com>
2022-09-02 14:32:01 -0300

    fix warnings

--- client/src/layouts/dashboard/index.js
+++ client/src/layouts/dashboard/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, lazy } from 'react';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import DashboardSidebar from './DashboardSidebar';
