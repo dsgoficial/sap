754d6931c85a6193b203f5907c2bc74bd5d4d858
Felipe Diniz <diniz.ime@gmail.com>
2023-05-04 20:06:51 -0300

    small rewrite

--- server/src/utils/async_handler.js
+++ server/src/utils/async_handler.js
@@ -3,23 +3,29 @@
 const BetterQueue = require('better-queue');
 
 const processingQueue = new BetterQueue((task, cb) => {
-  task.fn(task.req, task.res, task.next).then(() => cb(null, 'success')).catch(cb);
+  task.fn(task.req, task.res, task.next)
+    .then(() => cb(null, 'success'))
+    .catch(cb);
 });
 
-const asyncHandler = (fn) => (req, res, next) => {
+const enqueueTask = (fn, req, res, next) => {
+  return new Promise((resolve, reject) => {
+    processingQueue.push({fn, req, res, next}, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+const asyncHandler = fn => (req, res, next) => {
   if (req.method === 'GET') {
     return Promise.resolve(fn(req, res, next)).catch(next);
   } else {
-    processingQueue.push(
-      { fn, req, res, next },
-      (err, result) => {
-        if (err) {
-          return next(err);
-        }
-      }
-    );
+    return Promise.resolve(enqueueTask( fn, req, res, next)).catch(next);
   }
 };
-
 
 module.exports = asyncHandler
